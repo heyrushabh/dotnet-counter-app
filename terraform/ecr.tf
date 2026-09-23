63804aeac265966f8d771defc43ebe3281c48eea
Rushabh <rushabh1199@outlook.com>
2026-09-23 20:43:41 +1200

    Add ECR lifecycle policies and image versioning

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -17,3 +17,44 @@ resource "aws_ecr_repository" "Frontend" {
 
   tags = local.common_tags
 }
+
+resource "aws_ecr_lifecycle_policy" "api" {
+  repository = aws_ecr_repository.api.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "keep last 3 API images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+resource "aws_ecr_lifecycle_policy" "frontend" {
+  repository = aws_ecr_repository.Frontend.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "keep last 3 Frontend images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
